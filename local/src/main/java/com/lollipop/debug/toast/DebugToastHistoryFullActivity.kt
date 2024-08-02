f7266d1dc47ed864a19e35415d48ba3f27d04bba
Lollipop <liujian594@hotmail.com>
2024-08-02 23:30:33 +0800

    feat: 做了Toast历史页面的框架事件，发现一些好玩的写法

--- local/src/main/java/com/lollipop/debug/toast/DebugToastHistoryFullActivity.kt
+++ local/src/main/java/com/lollipop/debug/toast/DebugToastHistoryFullActivity.kt
@@ -1,14 +1,19 @@
 package com.lollipop.debug.toast
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import com.lollipop.debug.local.R
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import com.lollipop.debug.local.databinding.ActivityDebugToastHistoryFullBinding
 
 class DebugToastHistoryFullActivity : AppCompatActivity() {
@@ -29,14 +34,80 @@ class DebugToastHistoryFullActivity : AppCompatActivity() {
         ActivityDebugToastHistoryFullBinding.inflate(layoutInflater)
     }
 
+    private val backPressedWithSearch = object : OnBackPressedCallback(false) {
+        override fun handleOnBackPressed() {
+            cleanSearch()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true
+        }
+        initSearchBar()
+        initContentView()
+    }
+
+    private fun initSearchBar() {
+        binding.backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+        onBackPressedDispatcher.addCallback(backPressedWithSearch)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.actionBar) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+        binding.searchInputView.setOnFocusChangeListener { v, hasFocus ->
+            updateSearchBarIcon()
+        }
+        binding.searchInputView.doOnTextChanged { text, start, before, count ->
+            updateSearchBarIcon()
+            onSearchInfoChanged()
+        }
+        binding.cleanSearchButton.setOnClickListener {
+            cleanSearch()
+        }
+        binding.requestSearchButton.isFocusable = false
+    }
+
+    private fun hideKeyboard(view: View) {
+        val windowController = WindowCompat.getInsetsController(window, view)
+        windowController.hide(WindowInsetsCompat.Type.ime())
+//        return
+//        val imm = ViewUtils.getInputMethodManager(view)
+//        imm?.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun cleanSearch() {
+        binding.searchInputView.setText("")
+        binding.searchInputView.clearFocus()
+        hideKeyboard(binding.searchInputView)
+    }
+
+    private fun updateSearchBarIcon() {
+        val hasFocus = binding.searchInputView.hasFocus()
+        val info = binding.searchInputView.text ?: ""
+        val needClean = hasFocus || info.isNotEmpty()
+        binding.cleanSearchButton.isVisible = needClean
+        binding.requestSearchButton.isVisible = !needClean
+        backPressedWithSearch.isEnabled = needClean
+    }
+
+    private fun onSearchInfoChanged() {
+        // TODO()
+    }
+
+    private fun initContentView() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.toastListView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
 }
